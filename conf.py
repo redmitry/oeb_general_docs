56aa1d3ef513a74512be7de3c277c7baf608a55f
javi <javier.garrayo@hotmail.com>
2020-11-25 13:45:33 +0100

    docs first version

--- conf.py
+++ conf.py
@@ -54,6 +54,12 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_js_files = ['js/expand_tabs.js']
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
